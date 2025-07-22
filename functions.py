b4e5ee0fc97ff5bb75837a6bcb99918ec2e52c94
doyle-beep <doylear0@sewanee.edu>
2025-07-22 15:25:14 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,15 @@
+
+from openai import OpenAI #type: ignore
+client = OpenAI(
+    api_key = 'SECRET_KEY'
+)
+
+
+def prompt(text):
+    completion = client.responses.create(
+        model="gpt-4.1-mini",
+        input=text
+    )
+    answer = completion.output_text
+    print(answer)
+    return answer
---
+++ llm.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for, request #type: ignore
+from flask_bootstrap import Bootstrap5 #type: ignore
+
+from flask_wtf import FlaskForm, CSRFProtect #type: ignore
+from wtforms import StringField, SubmitField #type: ignore
+
+from functions import prompt #type: ignore
+
+app = Flask(__name__)
+app.secret_key = 'tO$&!|0wkamvVia0?n$I'
+
+bootstrap = Bootstrap5(app)
+csrf = CSRFProtect(app)
+
+class AskForm(FlaskForm):
+    p = StringField('Ask me anything! Or tell me to do something.')
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def ppt():
+    form = AskForm()
+    return render_template('index.html', form=form)
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    if request.method == 'POST':
+        pt = request.form['p']
+        answer = prompt(pt)
+    return render_template('answer.html', pt=pt, answer=answer)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002, debug=True)
