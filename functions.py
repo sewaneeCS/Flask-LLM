68a1638f8966ac85b9bf9f1ddd002b518252d227
doyle-beep <doylear0@sewanee.edu>
2025-07-24 12:07:33 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,12 +3,16 @@ from openai import OpenAI #type: ignore
 client = OpenAI(
     api_key = 'SECRET_KEY'
 )
+mdl = "gpt-4.1-mini"
 
 
 def prompt(text):
     completion = client.responses.create(
-        model="gpt-4.1-mini",
+        model=mdl,
         input=text
     )
     answer = completion.output_text
     return answer
+
+def getmodel():
+    return mdl
